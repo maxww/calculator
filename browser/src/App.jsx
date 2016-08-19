4207379800a949fb0ffc50495b8d7b92d2559739
Taffy Chen <tornglinchen@gmail.com>
2016-08-18 22:40:56 -0400

    added bootstrap

--- browser/src/App.jsx
+++ browser/src/App.jsx
@@ -11,9 +11,13 @@ export default class App extends React.Component {
 	render() {
 		let updateViewwindow = this.updateViewwindow.bind(this)
 		return (
-			<div>
-				<h4>Calculator App</h4>
-				<Calculator buttons={this.state.buttons} viewwindow={this.state.viewwindow} updateViewwindow={updateViewwindow}/>
+			<div className="row">
+				<div className="col-lg-4 col-sm-12"></div>
+				<div className="col-lg-4 col-sm-12">
+					<h4>Calculator App</h4>
+					<Calculator buttons={this.state.buttons} viewwindow={this.state.viewwindow} updateViewwindow={updateViewwindow}/>
+				</div>
+				<div className="col-lg-4 col-sm-12"></div>
 			</div>
 		)
 	}
